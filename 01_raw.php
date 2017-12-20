a235b9a40508e4f7f5b97f9e6ca5e9d32389bb6f
kiang <kiange@gmail.com>
2017-12-20 10:06:23 +0800

    first batch

---
+++ 01_raw.php
@@ -0,0 +1,104 @@
+<?php
+$layers = array(
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/0' => 'FHID.SDE.ABA0108000011054_A臺北市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/1' => 'FHID.SDE.ABA0108000021054_B臺中市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/2' => 'FHID.SDE.ABA0108000031054_C基隆市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/3' => 'FHID.SDE.ABA0108000041054_D臺南市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/4' => 'FHID.SDE.ABA0108000051054_E高雄市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/5' => 'FHID.SDE.ABA0108000061054_F新北市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/6' => 'FHID.SDE.ABA0108000071054_G宜蘭縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/7' => 'FHID.SDE.ABA0108000081054_H桃園市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/8' => 'FHID.SDE.ABA0108000091054_I嘉義市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/9' => 'FHID.SDE.ABA0108000101054_J新竹縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/10' => 'FHID.SDE.ABA0108000111054_K',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/11' => 'FHID.SDE.ABA0108000121054_M南投縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/12' => 'FHID.SDE.ABA0108000131054_N彰化縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/13' => 'FHID.SDE.ABA0108000141054_O新竹市',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/14' => 'FHID.SDE.ABA0108000151054_P雲林縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/15' => 'FHID.SDE.ABA0108000161054_Q嘉義縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/16' => 'FHID.SDE.ABA0108000171054_X澎湖縣119',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/17' => 'FHID.SDE.ABA0108000181054_U花蓮縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/18' => 'FHID.SDE.ABA0108000191054_V臺東縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/19' => 'FHID.SDE.ABA0108000201054_T屏東縣',
+  'BA/new%E5%9C%B0%E7%B1%8D%E5%9C%96_%E6%9F%A5%E8%A9%A2%E7%94%A82016/MapServer/20' => 'FHID.SDE.全國段外圍圖_有澎湖',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/26' => '第二輪檢訂林區道路圖',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/27' => '運研所_橋樑',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/28' => '運研所_隧道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/29' => '運研所_捷運',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/30' => '運研所_高鐵',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/31' => '阿里山鐵路',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/32' => '運研所_鐵路1',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/33' => '運研所_鐵路2',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/35' => '國道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/36' => '國道別稱',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/37' => '國道附屬道路',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/38' => '國道附屬道路別稱',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/39' => '省道快速道路',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/40' => '省道快速道路別稱',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/41' => '省道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/42' => '省道別稱',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/43' => '縣道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/44' => '縣道別稱',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/45' => '鄉道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/46' => '鄉道別稱',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/47' => '產業道路',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/48' => '市區快速道路',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/49' => '市區道路(路街)',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/50' => '市區道路(巷弄)',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/51' => '其它',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/53' => '國道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/55' => '國道',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/56' => '野生動物保護區',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/57' => '野生動物重要棲息環境',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/58' => '自然保護區',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/59' => '自然保留區',
+  'FH/%E6%9E%97%E5%8B%99%E5%B1%80%E9%9B%BB%E5%AD%90%E5%9C%B0%E5%9C%96/MapServer/62' => '國家公園範圍界',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/2' => '水庫集水區0991',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/3' => '森林遊樂區範圍界',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/4' => '全台保安林概略圖',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/5' => '全台保安林分布圖',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/6' => '國有林造林地籍圖',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/7' => '第二輪檢訂小班圖',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/8' => '國家公園範圍',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/9' => '野生動物重要棲息環境',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/10' => '自然保留區',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/11' => '自然保護區',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/12' => '野生動物保護區',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/13' => '國家風景區1021',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/14' => '自來水水質水量保護區1011',
+  'FH/%E7%A4%A6%E6%A5%AD%E7%94%A8%E5%9C%B0/MapServer/15' => 'DTM_Value',
+);
+foreach($layers AS $layerUrl => $layerName) {
+  $layerId = urldecode(str_replace('/', '_', $layerUrl));
+  $idFile = __DIR__ . '/raw/' . $layerId . 'Id';
+  $layerPath = __DIR__ . '/raw/' . $layerId;
+  if(!file_exists($layerPath)) {
+    mkdir($layerPath, 0777, true);
+  }
+  if(!file_exists($idFile)) {
+    file_put_contents($idFile, '0');
+  }
+  $lastId = intval(file_get_contents($idFile));
+  $objects = array();
+
+  while(++$lastId) {
+    $objects[] = $lastId;
+    if($lastId % 200 === 0) {
+      $targetFile = $layerPath . '/data_' . $lastId . '.json';
+      if(!file_exists($targetFile)) {
+        $q = implode(',', $objects);
+        $json = gzdecode(shell_exec("curl -k 'http://gis.forest.gov.tw/arcgis/rest/services/{$layerUrl}/query?objectIds={$q}&outFields=*&returnGeometry=true&f=json' -H 'Host: gis.forest.gov.tw' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Referer: http://gis.forest.gov.tw/arcgis/rest/services/' -H 'Connection: keep-alive'"));
+        $obj = json_decode($json, true);
+        if(!isset($obj['features'][0])) {
+          file_put_contents($idFile, $lastId);
+          echo "{$layerId} done";
+          break;
+        }
+        echo "processing {$layerId}/{{$lastId}}\n";
+        file_put_contents($targetFile, $json);
+      }
+      $objects = array();
+      file_put_contents($idFile, $lastId);
+    }
+  }
+}
